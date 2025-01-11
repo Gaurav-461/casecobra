73a3669d003337e611fd3f0db6bb2f2675b3310f
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-01-11 22:15:47 +0530

    create index file for component exports

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Phone from "./Phone";
+import Navbar from "./Navbar";
+import { Icons } from "./Icons";
+import Reviews from "./Reviews";
+
+export { MaxWidthWrapper, Phone, Navbar, Icons, Reviews };
