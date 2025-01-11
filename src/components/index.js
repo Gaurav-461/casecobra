ff2257c25cc41f2d5797b52099af1283eb0bfc2c
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-01-12 02:48:48 +0530

    feat(index.js): add Footer component to exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ import Phone from "./Phone";
 import Navbar from "./Navbar";
 import { Icons } from "./Icons";
 import Reviews from "./Reviews";
-
-export { MaxWidthWrapper, Phone, Navbar, Icons, Reviews };
+import Footer from "./Footer";
+export { MaxWidthWrapper, Phone, Navbar, Icons, Reviews, Footer };
