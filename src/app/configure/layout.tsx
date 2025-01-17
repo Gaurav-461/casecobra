469f07c0b568faa6132ba78688d37725d7359ba2
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-01-18 02:53:14 +0530

    feat(layout): create Layout component to wrap children with MaxWidthWrapper and Steps
    - Introduced a new Layout component that integrates the MaxWidthWrapper and Steps components.
    - The Layout component provides a structured layout for child components, enhancing the overall user interface.
    - This addition improves the organization of the application and maintains consistency in the design.

---
+++ src/app/configure/layout.tsx
@@ -0,0 +1,13 @@
+import { MaxWidthWrapper, Steps } from "@/components";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <MaxWidthWrapper className="flex flex-1 flex-col">
+      <Steps />
+      {children}
+    </MaxWidthWrapper>
+  );
+};
+
+export default Layout;
