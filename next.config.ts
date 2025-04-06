1ec1e4a8317a9653992d1d4d9ecee4a50d9d3cd6
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-04-06 14:27:23 +0530

    feat: Add CORS headers configuration to next.config.ts for API routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers(){
+    return [
+      {
+        source: "/api/:path*",
+        headers:[
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ]
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
@@ -17,7 +37,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Temporary fix to bypass build errors
   },
-  crossOrigin: "anonymous",
 };
 
 export default nextConfig;
