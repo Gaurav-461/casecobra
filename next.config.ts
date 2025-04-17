9c7d42894e746ffce1747f986678096c1db21c3a
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-04-18 01:07:42 +0530

    refactor: remove CORS headers configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://casecobra-rouge-delta.vercel.app, https://casecobra-rouge-delta.vercel.app/api/:path*", // Set your origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
